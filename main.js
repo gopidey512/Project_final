7c1b12560b3dac1224d13b40d12e3637ec9b6b95
GOPINATH DEY <98454940+gopidey512@users.noreply.github.com>
2024-01-25 21:43:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
+/**
+   * Mobile nav toggle
+   */
+ const mobileNavToogle = document.querySelector('.mobile-nav-toggle');
+ if (mobileNavToogle) {
+   mobileNavToogle.addEventListener('click', function(event) {
+     event.preventDefault();
 
+     document.querySelector('body').classList.toggle('mobile-nav-active');
+
+     this.classList.toggle('bi-list');
+     this.classList.toggle('bi-x');
+   });
+ }
+
+ /**
+  * Toggle mobile nav dropdowns
+  */
+ const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
+
+ navDropdowns.forEach(el => {
+   el.addEventListener('click', function(event) {
+     if (document.querySelector('.mobile-nav-active')) {
+       event.preventDefault();
+       this.classList.toggle('active');
+       this.nextElementSibling.classList.toggle('dropdown-active');
+
+       let dropDownIndicator = this.querySelector('.dropdown-indicator');
+       dropDownIndicator.classList.toggle('bi-chevron-up');
+       dropDownIndicator.classList.toggle('bi-chevron-down');
+     }
+   })
+ });
